9f1e94bb446cb8152b408f4ba68747d72b973f66
mbf <mmarvb8@gmail.com>
2021-04-19 03:45:56 -0400

    using vuejs to generate institution list. need to hook up data

--- Banksyncwiz/Pages/FinancialInstitutionsList.cshtml.cs
+++ Banksyncwiz/Pages/FinancialInstitutionsList.cshtml.cs
@@ -17,9 +17,34 @@ namespace Banksyncwiz.Pages
             _logger = logger;
         }
 
+        public int countx { get; set; }
+        public int institutionList { get; set; }
+
         public void OnGet()
         {
+            Console.Out.WriteLine("In FI List Get().");
+            this.countx = 2;
+        }
+
+        public void OnGetInstitutions()
+        {
+            Console.Out.WriteLine("In OnGetInstitutionList().");
+        }
+
+        public void OnPostInstitutions()
+        {
+            Console.Out.WriteLine("In OnPostInstitutionList().");
         }
     }
 }
 
+// Putting vuejs on this page.
+// https://limelightmarketing.com/blogs/vue-asp-net-core-razor-pages/
+// https://ml-software.ch/posts/using-vuejs-as-a-drop-in-component
+// https://markus.oberlehner.net/blog/goodbye-webpack-building-vue-applications-without-webpack/
+//
+// Info on vuejs for prod.
+// This is the message displayed when i'm running current Vue via script tag.
+//
+// You are running a development build of Vue.
+// Make sure to use the production build (*.prod.js) when deploying for production.
